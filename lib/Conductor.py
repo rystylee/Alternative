236b6b99de803f09aaadadab4e038a18b9652309
rystylee <rystylee8@gmail.com>
2017-12-17 11:50:50 +0900

    Added Queue

--- lib/Conductor.py
+++ lib/Conductor.py
@@ -17,11 +17,21 @@ class Conductor(object):
 
 
 players = []
-p1 = Player(messages=["kick01", "amp", 1.2, "decay", 0.5])
-p2 = Player(messages=["snare01", "amp", 1.0, "decay", 0.5])
-p3 = Player(messages=["clap01", "amp", 0.8])
-p4 = Player(messages=["hat01", "amp", 1.0])
+p1 = Player(messages=["acid_oto309", "amp", 1.0], original_durations=[])
+p2 = Player(messages=["cymbal808_ryan", "amp", 1.0], original_durations=[])
+p3 = Player(messages=["ping_mh", "amp", 0.8], original_durations=[])
+p4 = Player(messages=["snare_stein", "amp", 1.0], original_durations=[])
 players.append(p1)
 players.append(p2)
 players.append(p3)
 players.append(p4)
+
+# players = []
+# p1 = Player(messages=["kick01", "amp", 1.2, "decay", 0.5])
+# p2 = Player(messages=["snare01", "amp", 1.0, "decay", 0.5])
+# p3 = Player(messages=["clap01", "amp", 0.8])
+# p4 = Player(messages=["hat01", "amp", 1.0])
+# players.append(p1)
+# players.append(p2)
+# players.append(p3)
+# players.append(p4)
--- lib/Player.py
+++ lib/Player.py
@@ -5,15 +5,20 @@ from .Queue import Queue
 
 
 class Player(object):
-    def __init__(self, messages, clock_interval=1.0):
+    def __init__(self, messages, original_durations, clock_interval=1.0):
         self.messages = messages
+        self.original_durations = original_durations
         self.clock_interval = clock_interval
+
         self.num_notes = 1
         self.durations = Queue()
         self.next_durations = []
+        self.durations.reset_queue(self.original_durations)
 
 
     def play(self, interval):
+        self.forward_durations()
+
         if self.next_durations == []:
             self.num_notes = 1
         else:
@@ -21,13 +26,25 @@ class Player(object):
 
         if self.num_notes == 1:
             if not self.next_durations == []:
-                time.sleep(self.next_durations[0])
+                time.sleep(self.next_durations[0] * interval)
             send_msg(self.messages)
         else:
             for i in range(len(self.next_durations)):
-                time.sleep(self.next_durations[i])
-                print(self.next_durations[i])
+                time.sleep(self.next_durations[i] * interval)
                 send_msg(self.messages)
+
+
+    def forward_durations(self):
+        if self.durations.is_Empty():
+            self.durations.reset_queue(*self.original_durations)
+
+        self.set_next_durations(self.durations.dequeue())
+
+
+    def set_original_durations(self, *original_durations):
+        self.original_durations.clear()
+        for dur in original_durations:
+            self.original_durations.append(dur)
 
 
     def set_durations(self, durations):
