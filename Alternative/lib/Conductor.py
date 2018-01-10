8b55fc99a72e70b447b92edf597c1b2b9200547f
rystylee <rystylee8@gmail.com>
2018-01-10 14:28:46 +0900

    Cleaned

--- Alternative/lib/Conductor.py
+++ Alternative/lib/Conductor.py
@@ -25,15 +25,15 @@ class Conductor(object):
 
 
 players = []
-#p1 = RhythmicPlayer(messages=["kick01"], original_durations=[[1/2,1/2],[1/4,1/2]])
-#p2 = RhythmicPlayer(messages=["snare01"], original_durations=[[1/4,1/4,1/4,1/4],[1/2]])
-#p1 = LSystemPlayer(messages=["kick01"], LNum_iter=5, LSeed=[1/2, 1/8], original_durations=[])
+#p1 = RhythmicPlayer(messages=["kick"], original_durations=[[1/2,1/2],[1/4,1/2]])
+#p2 = RhythmicPlayer(messages=["snare"], original_durations=[[1/4,1/4,1/4,1/4],[1/2]])
+#p1 = LSystemPlayer(messages=["kick"], LNum_iter=5, LSeed=[1/2, 1/8], original_durations=[])
 p1 = LSystemPlayer(messages=["ckick"], LNum_iter=5, LSeed=[1/2, 1/8], original_durations=[])
-p2 = LSystemPlayer(messages=["snare01"], LNum_iter=5, LSeed=[1/4], original_durations=[])
-#p3 = LSystemPlayer(messages=["clap01"], LNum_iter=5, LSeed=[1/8], original_durations=[])
-#p4 = LSystemPlayer(messages=["hat01"], LNum_iter=4, LSeed=[1/3, 1/4], original_durations=[])
-p5 = LSystemPlayer(messages=["pad01", "amp", "440"], LNum_iter=3, LSeed=[1/4, 1/8], original_durations=[])
-p6 = LSystemPlayer(messages=["fmchord01"], LNum_iter=4, LSeed=[1/16], original_durations=[])
+p2 = LSystemPlayer(messages=["snare"], LNum_iter=5, LSeed=[1/4], original_durations=[])
+#p3 = LSystemPlayer(messages=["clap"], LNum_iter=5, LSeed=[1/8], original_durations=[])
+#p4 = LSystemPlayer(messages=["hat"], LNum_iter=4, LSeed=[1/3, 1/4], original_durations=[])
+p5 = LSystemPlayer(messages=["pad", "amp", "440"], LNum_iter=3, LSeed=[1/4, 1/8], original_durations=[])
+p6 = LSystemPlayer(messages=["fmchord"], LNum_iter=4, LSeed=[1/16], original_durations=[])
 players.append(p1)
 players.append(p2)
 #players.append(p3)
